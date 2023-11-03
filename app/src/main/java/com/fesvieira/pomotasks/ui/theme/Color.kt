1ce14181a204f91e5cc26ee3a92bd689af6741be
felipevieira <felipe2204sv@gmail.com>
2023-11-03 15:22:12 -0300

    add colors and theme

--- app/src/main/java/com/fesvieira/pomotasks/ui/theme/Color.kt
+++ app/src/main/java/com/fesvieira/pomotasks/ui/theme/Color.kt
@@ -2,10 +2,86 @@ package com.fesvieira.pomotasks.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+/**
+ * Light
+ **/
+val mtl_primary = Color(0xFF3B5BA9)
+val mtl_onPrimary = Color(0xFFFFFFFF)
+val mtl_primaryContainer = Color(0xFFDAE2FF)
+val mtl_onPrimaryContainer = Color(0xFF001848)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val mtl_secondary = Color(0xFF006C4C)
+val mtl_onSecondary = Color(0xFFFFFFFF)
+val mtl_secondaryContainer = Color(0xFF7AFAC3)
+val mtl_onSecondaryContainer = Color(0xFF002114)
+
+val mtl_tertiary = Color(0xFF934B00)
+val mtl_onTertiary = Color(0xFFFFFFFF)
+val mtl_tertiaryContainer = Color(0xFFFFDCC5)
+val mtl_onTertiaryContainer = Color(0xFF301400)
+
+val mtl_error = Color(0xFFBA1A1A)
+val mtl_errorContainer = Color(0xFFFFDAD6)
+val mtl_onError = Color(0xFFFFFFFF)
+val mtl_onErrorContainer = Color(0xFF410002)
+
+val mtl_background = Color(0xFFFEFBFF)
+val mtl_onBackground = Color(0xFF1B1B1F)
+
+val mtl_surface = Color(0xFFFEFBFF)
+val mtl_onSurface = Color(0xFF1B1B1F)
+val mtl_surfaceVariant = Color(0xFFE1E2EC)
+val mtl_onSurfaceVariant = Color(0xFF45464F)
+
+val mtl_outline = Color(0xFF757780)
+
+val mtl_inverseOnSurface = Color(0xFFF2F0F4)
+val mtl_inverseSurface = Color(0xFF303034)
+val mtl_inversePrimary = Color(0xFFB2C5FF)
+
+val mtl_surfaceTint = Color(0xFF3B5BA9)
+val mtl_outlineVariant = Color(0xFFC5C6D0)
+val mtl_scrim = Color(0xFF000000)
+
+
+/**
+ * Dark
+ **/
+val mtd_primary = Color(0xFFB2C5FF)
+val mtd_onPrimary = Color(0xFF002B73)
+val mtd_primaryContainer = Color(0xFF1F428F)
+val mtd_onPrimaryContainer = Color(0xFFDAE2FF)
+
+val mtd_secondary = Color(0xFF5BDDA8)
+val mtd_onSecondary = Color(0xFF003826)
+val mtd_secondaryContainer = Color(0xFF005138)
+val mtd_onSecondaryContainer = Color(0xFF7AFAC3)
+
+val mtd_tertiary = Color(0xFFFFB782)
+val mtd_onTertiary = Color(0xFF4F2500)
+val mtd_tertiaryContainer = Color(0xFF703800)
+val mtd_onTertiaryContainer = Color(0xFFFFDCC5)
+
+val mtd_error = Color(0xFFFFB4AB)
+val mtd_errorContainer = Color(0xFF93000A)
+val mtd_onError = Color(0xFF690005)
+val mtd_onErrorContainer = Color(0xFFFFDAD6)
+
+val mtd_background = Color(0xFF1B1B1F)
+val mtd_onBackground = Color(0xFFE4E2E6)
+
+val mtd_surface = Color(0xFF1B1B1F)
+val mtd_onSurface = Color(0xFFE4E2E6)
+
+val mtd_surfaceVariant = Color(0xFF45464F)
+val mtd_onSurfaceVariant = Color(0xFFC5C6D0)
+
+val mtd_outline = Color(0xFF8F909A)
+
+val mtd_inverseOnSurface = Color(0xFF1B1B1F)
+val mtd_inverseSurface = Color(0xFFE4E2E6)
+val mtd_inversePrimary = Color(0xFF3B5BA9)
+
+val mtd_surfaceTint = Color(0xFFB2C5FF)
+val mtd_outlineVariant = Color(0xFF45464F)
+val mtd_scrim = Color(0xFF000000)
