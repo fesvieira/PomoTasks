70a7b7e37512a396737461152919a1ac753363fd
felipevieira <felipe2204sv@gmail.com>
2023-11-03 17:26:20 -0300

    refactor and fix minor ui issues

---
+++ app/src/main/java/com/fesvieira/pomotasks/ui/screens/MainScreen.kt
@@ -0,0 +1,65 @@
+package com.fesvieira.pomotasks.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.fesvieira.pomotasks.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Scaffold(
+            floatingActionButton = {
+                AppFloatActionButton(icon = painterResource(R.drawable.ic_add)) {
+
+                }
+            }
+        ) {
+            Column(modifier = Modifier
+                .padding(it)
+                .padding(16.dp)
+                .fillMaxSize()) {
+                Text(text = "Felipe")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMainScreen() {
+    MainScreen()
+}
+
+@Composable
+fun AppFloatActionButton(icon: Painter, onClick: () -> Unit) {
+    FloatingActionButton(
+        containerColor = MaterialTheme.colorScheme.tertiaryContainer,
+        shape = RoundedCornerShape(16.dp),
+        onClick = onClick
+    ) {
+        Icon(
+            painter = icon,
+            tint = MaterialTheme.colorScheme.onTertiaryContainer,
+            contentDescription = null,
+        )
+    }
+}
