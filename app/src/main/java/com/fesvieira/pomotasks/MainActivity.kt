4763465b26426962da085e943f7b356991427ff6
felipevieira <felipe2204sv@gmail.com>
2023-11-03 16:40:12 -0300

    create main screen

--- app/src/main/java/com/fesvieira/pomotasks/MainActivity.kt
+++ app/src/main/java/com/fesvieira/pomotasks/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.fesvieira.pomotasks.ui.screens.MainScreen
 import com.fesvieira.pomotasks.ui.theme.PomoTasksTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +23,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                     MainScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PomoTasksTheme {
-        Greeting("Android")
     }
 }
